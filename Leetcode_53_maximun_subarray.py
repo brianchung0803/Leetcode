f2eac8848361769ea28fa121caa455fb030a9099
brianchung <Brian@Veronicade-MacBook-Pro.local>
2020-01-21 22:44:08 +0800

    add number 53

---
+++ Leetcode_53_maximun_subarray.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if(len(nums)==0):
+            return 0
+        if len(nums)==1:
+            return nums[0]
+        
+        ans=nums[0]
+        num=nums[0]
+        for i in nums[1:]:
+            num=max(i,num+i)
+            ans=max(num,ans)
+        return ans
