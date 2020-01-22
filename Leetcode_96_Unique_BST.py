eef3f1149e1a8c3b88347a48bc10e5c5becf83ae
brianchung0803 <lalala850803@gmail.com>
2020-01-22 17:23:30 +0800

    87.9&: Dynamic Programming

--- Leetcode_96_Unique_BST.py
+++ Leetcode_96_Unique_BST.py
@@ -7,17 +7,16 @@ class Solution(object):
         if n==0:
             return 0
         
-        num=[]
-        for i in range(n+1):
-            num.append(0)
+        num=[1]
         
-        num[0]=1
         for j in range(1,n+1):
             left=j-1
             right=0
+            tmp=0
             while(left>=0):
-                num[j]+=num[left]*num[right]
+                tmp+=num[left]*num[right]
                 left-=1
                 right+=1
+            num.append(tmp)
     
         return num[n]
